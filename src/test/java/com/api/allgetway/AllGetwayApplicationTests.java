1c476413a98c5818ee2c6abaa26108f1a75b3cec
anand9773 <aaanand9773@gmail.com>
2024-04-27 19:36:50 +0530

    project is created api-gateway for all

---
+++ src/test/java/com/api/allgetway/AllGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.allgetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllGetwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
